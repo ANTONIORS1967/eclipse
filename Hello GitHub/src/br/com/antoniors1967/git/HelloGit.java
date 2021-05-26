1eda524bba3ddab000f3c1783871ef2ce1f58f56
ANTONIO RODRIGUES DOS SANTOS <43083481+ANTONIORS1967@users.noreply.github.com>
2021-05-25 22:28:54 -0300

    Adicionei arquivo via upload

---
+++ Hello GitHub/src/br/com/antoniors1967/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.antoniors1967.git;
+
+/**
+ * @author anton
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
